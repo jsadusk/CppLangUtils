14f84eb30cf2ff2a715d521eba7b62cfae7e4ab8
Joseph Sadusk <jsadusk@gmail.com>
2018-02-20 23:05:04 -0800

    Started the iterator library with a dynamic iterator, an iterator that preserves the value type, but erases the type of its container

---
+++ include/lang_utils/iterator.h
@@ -0,0 +1,141 @@
+#ifndef LANG_UTILS_ITERATOR_H
+#define LANG_UTILS_ITERATOR_H
+
+#include <iterator>
+#include <type_traits>
+#include <cassert>
+#include <memory>
+
+namespace lang_utils {
+
+template <typename VALUE>
+class dynamic_iterator {
+public:
+    using value_type = typename std::decay<VALUE>::type;
+    using reference_type = typename std::add_lvalue_reference<value_type>::type;
+    using pointer_type = typename std::add_pointer<value_type>::type;
+
+    //for now, make this adaptable later
+    using iterator_category = std::forward_iterator_tag;
+
+private:
+    class wrapper_base {
+    public:
+        virtual wrapper_base* clone() = 0;
+        virtual void increment() = 0;
+        virtual void decrement() = 0;
+        virtual reference_type dereference() = 0;
+
+        //re-enable these when I deal with random access
+        //virtual int difference(const wrapper_base &other) const = 0;
+        //virtual void advance(const int offset) = 0;
+        virtual bool equal(const wrapper_base &other) const = 0;
+    };
+
+    template <typename BASE>
+    class wrapper_derived : public wrapper_base {
+    public:
+        wrapper_derived(BASE &&base) : m_base(base) {}
+        wrapper_derived(const BASE &base) : m_base(base) {}
+        
+        virtual wrapper_base* clone() {
+            return (wrapper_base*)new wrapper_derived<BASE>(m_base);
+        }
+        
+        virtual void increment() { ++m_base; }
+        virtual void decrement() { ++m_base; }
+        virtual reference_type dereference() { return *m_base; }
+
+        //re-enable these when I deal with random access
+        /*virtual int difference(const wrapper_base &other) {
+            const wrapper_derived<BASE> *other_p =
+                dynamic_cast<const wrapper_derived<BASE>*>(&other);
+            assert(other_p);
+            return m_base - other_p->m_base;
+            }*/
+
+        /*virtual void advance(const size_t offset) {
+           m_base += offset;
+           }*/
+
+        virtual bool equal(const wrapper_base &other) const {
+            const wrapper_derived<BASE> *other_p =
+                dynamic_cast<const wrapper_derived<BASE>*>(&other);
+
+            return other_p != nullptr && m_base == other_p->m_base;
+        }
+
+    private:
+        BASE m_base;
+    };
+
+    using wrapper_ptr = std::unique_ptr<wrapper_base>;
+public:
+
+    dynamic_iterator() : m_wrapper(nullptr) {}
+
+    dynamic_iterator(const dynamic_iterator &other)
+        : m_wrapper(other.m_wrapper->clone()) {}
+
+    dynamic_iterator(dynamic_iterator &&other)
+        : m_wrapper(std::move(other.m_wrapper)) {}
+        
+    template <typename BASE>
+    dynamic_iterator(BASE &&base)
+        : m_wrapper((wrapper_base*)
+                      new wrapper_derived<typename std::decay<BASE>::type>(
+                          std::forward<BASE>(base))) {}
+
+    dynamic_iterator& operator=(const dynamic_iterator &other) {
+        m_wrapper = wrapper_ptr(other.m_wrapper->clone());
+        return *this;
+    }
+    
+    dynamic_iterator& operator=(dynamic_iterator &&other) {
+        m_wrapper = std::move(other.m_wrapper);
+        return *this;
+    }
+
+    dynamic_iterator& operator++() {
+        m_wrapper->increment();
+        return *this;
+    }
+
+    dynamic_iterator& operator--() {
+        m_wrapper->decrement();
+        return *this;
+    }
+
+    reference_type operator*() {
+        return m_wrapper->dereference();
+    }
+
+    int operator-(const dynamic_iterator &other) const {
+        return m_wrapper->difference(*other->m_wrapper);
+    }
+
+    dynamic_iterator& operator+(const int offset) {
+        m_wrapper->advance(offset);
+        return *this;
+    }
+
+    bool operator==(const dynamic_iterator &other) const {
+        return m_wrapper->equal(*other.m_wrapper);
+    }
+    bool operator!=(const dynamic_iterator &other) const {
+        return !m_wrapper->equal(*other.m_wrapper);
+    }
+private:
+    wrapper_ptr m_wrapper;
+};
+
+template <typename BASE>
+dynamic_iterator<typename std::iterator_traits<BASE>::value_type>
+make_dynamic_iterator(BASE &&base) {
+    return dynamic_iterator<typename std::iterator_traits<BASE>::value_type>(
+               std::forward<BASE>(base));
+}
+
+}
+
+#endif
---
+++ src/unit_test/test_iterator.cpp
@@ -0,0 +1,61 @@
+#include <lang_utils/iterator.h>
+
+#define BOOST_TEST_MODULE test_iterator
+#include <boost/test/unit_test.hpp>
+
+#include <vector>
+#include <list>
+
+using namespace lang_utils;
+
+BOOST_AUTO_TEST_CASE(test_dynamic_iterator_basic) {
+    std::vector<int> one {1, 3, 5};
+    std::list<int> two;
+
+    two.push_back(2);
+    two.push_back(4);
+    two.push_back(6);
+    
+    auto oneIter = make_dynamic_iterator(one.begin());
+    auto twoIter = make_dynamic_iterator(two.begin());
+
+    BOOST_REQUIRE(oneIter == oneIter);
+    BOOST_REQUIRE(oneIter != twoIter);
+
+    std::vector<dynamic_iterator<int>> iters;
+    iters.emplace_back(one.begin());
+    iters.emplace_back(two.begin());
+
+    BOOST_REQUIRE(!(iters[0] == iters[1]));
+    BOOST_REQUIRE(iters[0] == dynamic_iterator<int>(one.begin()));
+    BOOST_REQUIRE(iters[1] == dynamic_iterator<int>(two.begin()));
+    
+    BOOST_REQUIRE_EQUAL(*iters[0], 1);
+    BOOST_REQUIRE_EQUAL(*iters[1], 2);
+
+    ++iters[0];
+    BOOST_REQUIRE_EQUAL(*iters[0], 3);
+    BOOST_REQUIRE_EQUAL(*iters[1], 2);
+
+    BOOST_REQUIRE(!(iters[0] == dynamic_iterator<int>(one.begin())));
+                          
+    ++iters[1];
+    BOOST_REQUIRE_EQUAL(*iters[0], 3);
+    BOOST_REQUIRE_EQUAL(*iters[1], 4);
+
+    ++iters[0];
+    BOOST_REQUIRE_EQUAL(*iters[0], 5);
+    BOOST_REQUIRE_EQUAL(*iters[1], 4);
+
+    ++iters[1];
+    BOOST_REQUIRE_EQUAL(*iters[0], 5);
+    BOOST_REQUIRE_EQUAL(*iters[1], 6);
+
+    ++iters[0];
+    BOOST_REQUIRE(iters[0] == dynamic_iterator<int>(one.end()));
+    BOOST_REQUIRE_EQUAL(*iters[1], 6);
+
+    ++iters[1];
+    BOOST_REQUIRE(iters[1] == dynamic_iterator<int>(two.end()));
+}
+
