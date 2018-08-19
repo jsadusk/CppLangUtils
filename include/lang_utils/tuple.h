aedde6a998fa992da08dbe05003fdb58d0270481
Sir Filipp The Infallible <fgelm01@gmail.com>
2018-08-19 17:49:01 -0400

    Restore the _i and _p variants of functions.

--- include/lang_utils/tuple.h
+++ include/lang_utils/tuple.h
@@ -36,82 +36,98 @@ auto slice_tuples(TUPLES&&... tuples) {
     return std::tie(std::get<N>(std::forward<TUPLES>(tuples))...);
 }
 
+enum class pos_kind {
+    none,
+    compile_time,
+    run_time
+};
 
-template <typename FUNC, size_t... INDS, typename... TUPLES>
+template <pos_kind PK, typename FUNC>
+auto adapt_impl(FUNC&& func) {
+    if constexpr (PK == pos_kind::run_time) {
+        return [&func]<size_t i>() {
+            return [&func]<typename... ARGS>(ARGS&&... args) -> decltype(auto) {
+                return std::forward<FUNC>(func)(i, std::forward<ARGS>(args)...);
+            };
+        };
+    } else if constexpr (PK == pos_kind::compile_time) {
+        return [&func]<size_t i>() {
+            return [&func]<typename... ARGS>(ARGS&&... args) -> decltype(auto) {
+                return std::forward<FUNC>(func).template operator()<i>(std::forward<ARGS>(args)...);
+            };
+        };
+    } else {
+        return [&func]<size_t>() {
+            return [&func]<typename... ARGS>(ARGS&&... args) -> decltype(auto) {
+                return std::forward<FUNC>(func)(std::forward<ARGS>(args)...);
+            };
+        };
+    }
+}
+
+template <pos_kind PK, typename FUNC, size_t... INDS, typename... TUPLES>
 void foreach_tuple_impl(FUNC&& func, std::index_sequence<INDS...>,
                         TUPLES&&... tuples) {
     static_cast<void>((... && (static_cast<void>(
-        std::apply(std::forward<FUNC>(func),
+        std::apply(adapt_impl<PK>(std::forward<FUNC>(func)).template operator()<INDS>(),
                    slice_tuples<INDS>(std::forward<TUPLES>(tuples)...))), true)));
 }
 
 template <typename FUNC, typename... TUPLES>
 void foreach_tuple(FUNC&& func, TUPLES&&... tuples) {
-    foreach_tuple_impl(std::forward<FUNC>(func),
-                       std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
-                       std::forward<TUPLES>(tuples)...);
-}
-
-template <typename FUNC, size_t... INDS, typename... TUPLES>
-void foreach_tuple_i_impl(FUNC&& func,
-                          std::index_sequence<INDS...>,
-                          TUPLES&&... tuples) {
-    static_cast<void>((... && (static_cast<void>(
-         std::apply(std::forward<FUNC>(func),
-                    std::tuple_cat(std::tuple(INDS),
-                                   slice_tuples<INDS>(std::forward<TUPLES>(tuples)...)))),
-         true)));
+    foreach_tuple_impl<pos_kind::none>(
+            std::forward<FUNC>(func),
+            std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
+            std::forward<TUPLES>(tuples)...);
 }
 
 template <typename FUNC, size_t... INDS, typename... TUPLES>
 void foreach_tuple_i(FUNC&& func, TUPLES&&... tuples) {
-    foreach_tuple_i_impl(std::forward<FUNC>(func),
-                         std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
-                         std::forward<TUPLES>(tuples)...);
+    foreach_tuple_impl<pos_kind::run_time>(
+            std::forward<FUNC>(func),
+            std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
+            std::forward<TUPLES>(tuples)...);
 }
-
-template <typename FUNC, typename ...TUPLES>
-class foreach_tuple_p_helper {
-    template <typename F, typename ...T>
-    friend void foreach_tuple_p(F&&, T&&...);
-    
-    void operator()(std::index_sequence<>, FUNC &&func,
-                    TUPLES&& ...tuples) { }
-
-    template <size_t POS, size_t ...INDICES>
-    void operator()(std::index_sequence<POS, INDICES...>, FUNC &&func,
-                  TUPLES&& ...tuples) {
-        func.template operator()<POS>(std::get<POS>(tuples)...);
-        (*this)(std::index_sequence<INDICES...>(),
-                std::forward<FUNC>(func),
-                std::forward<TUPLES>(tuples)...);
-    }
-    
-};
 
 template <typename FUNC, typename ...TUPLES>
 void foreach_tuple_p(FUNC &&func, TUPLES&& ...tuples) {
-    static const size_t size = tuple_sizes_equal<TUPLES...>::value;
-
-    foreach_tuple_p_helper<FUNC, TUPLES...>()(std::make_index_sequence<size>(),
-                                              std::forward<FUNC>(func),
-                                              std::forward<TUPLES>(tuples)...);
+    foreach_tuple_impl<pos_kind::compile_time>(
+            std::forward<FUNC>(func),
+            std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
+            std::forward<TUPLES>(tuples)...);
 }
 
-template <typename FUNC, size_t... INDS, typename... TUPLES>
+template <pos_kind PK, typename FUNC, size_t... INDS, typename... TUPLES>
 auto map_tuple_impl(FUNC&& func,
                     std::index_sequence<INDS...>,
                     TUPLES&&... tuples) {
     return std::make_tuple(
-            std::apply(std::forward<FUNC>(func),
+            std::apply(adapt_impl<PK>(std::forward<FUNC>(func)).template operator()<INDS>(),
                        slice_tuples<INDS>(std::forward<TUPLES>(tuples)...))...);
 }
 
 template <typename FUNC, typename ...TUPLES>
 auto map_tuple(FUNC &&func, TUPLES&& ...tuples) {
-    return map_tuple_impl(std::forward<FUNC>(func),
-                          std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
-                          std::forward<TUPLES>(tuples)...);
+    return map_tuple_impl<pos_kind::none>(
+        std::forward<FUNC>(func),
+        std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
+        std::forward<TUPLES>(tuples)...);
+}
+
+template <typename FUNC, typename ...TUPLES>
+auto map_tuple_i(FUNC &&func, TUPLES&& ...tuples) {
+    return map_tuple_impl<pos_kind::run_time>(
+        std::forward<FUNC>(func),
+        std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
+        std::forward<TUPLES>(tuples)...);
+}
+
+template <typename FUNC, typename ...TUPLES>
+auto map_tuple_p(FUNC &&func, TUPLES&& ...tuples) {
+    return map_tuple_impl<pos_kind::compile_time>(
+        std::forward<FUNC>(func),
+        std::make_index_sequence<tuple_sizes_equal_v<TUPLES...>>{},
+        std::forward<TUPLES>(tuples)...);
 }
 
 
